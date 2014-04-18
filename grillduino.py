11aa5625995935f508dcd24f07be8226d95a4fec
Casey Deen <deen@mpia.de>
2014-04-18 02:12:20 +0200

    more grillduino.py

--- grillduino.py
+++ grillduino.py
@@ -1,5 +1,13 @@
 import scipy
 import numpy
 import matplotlib.pyplot as pyplot
+import serial
 
 print("Welcome to Grillduino!  Connected grilling for the masses!")
+
+"""
+Using the voltages dropped across the different resisters, calculate the 
+resistance of each resistor.  R3 is the one in the heat.  Using a pre-stored
+calibration, conver the resistance of R3 to temperature.  Plot.  Upload plot
+to website.
+"""
